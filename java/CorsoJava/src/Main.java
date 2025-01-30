d685bf51cbc9069aad6fbf2b7b83d0ec20f53841
Daniel Salvador <daniel.salvador@rosabianca.tn.it>
2025-01-30 21:40:11 +0100

    Laboratorio java fatto in classe

--- java/CorsoJava/src/Main.java
+++ java/CorsoJava/src/Main.java
@@ -1,15 +1,31 @@
+import Lab1.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hello and welcome!");
+
+        Animale pippo = new Animale("Pippo");
+        System.out.println("Il nome del oggetto pippo è :"+pippo.getNome());
+        pippo.setNome("PIPPO");
+        System.out.println("Il nome del oggetto pippo  ora è :"+pippo.getNome());
+
+        System.out.println(pippo);
+
+        pippo.eatFood(500);
+        System.out.println(pippo);
+        pippo.goToWalk(10);
+        System.out.println(pippo);
+
+        Animale pluto = new Animale("pluto",10,100,false);
+        System.out.println(pluto);
+
+
+
+
+
+
     }
 }
