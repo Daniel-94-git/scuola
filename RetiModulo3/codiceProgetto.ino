96d795cf7f8ddb5c6569e2e95e981f0761a7db04
Daniel Salvador <daniel.salvador@rosabianca.tn.it>
2025-03-14 20:40:24 +0100

    Modulo 3 sistemi e reti

---
+++ RetiModulo3/codiceProgetto.ino
@@ -0,0 +1,141 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  // numero 1  
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  
+  // numero 2
+  digitalWrite(12,HIGH);
+  delay(3000);
+  digitalWrite(12,LOW);
+  delay(1000);
+  
+  // numero 3
+  digitalWrite(12,HIGH);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(12,LOW);
+  digitalWrite(13,LOW);
+  delay(1000);
+  
+  // numero 4
+  digitalWrite(11,HIGH);
+  delay(3000);
+  digitalWrite(11,LOW);
+  delay(1000);
+  
+  // numero 5
+  digitalWrite(11,HIGH);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(11,LOW);
+  digitalWrite(13,LOW);
+  delay(1000);
+  
+  // numero 6
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  delay(3000);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  delay(1000);
+  
+  // numero 7
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  delay(1000);
+  
+  // numero 8
+  digitalWrite(10,HIGH);
+  delay(3000);
+  digitalWrite(10,LOW);
+  delay(1000);
+  
+  // numero 9
+  digitalWrite(10,HIGH);
+  digitalWrite(13,HIGH);
+  delay(3000);
+  digitalWrite(10,LOW);
+  digitalWrite(13,LOW);
+  delay(1000);
+  
+  // numero 10
+  digitalWrite(12,HIGH);
+  digitalWrite(10,HIGH);
+  delay(3000);
+  digitalWrite(12,LOW);
+  digitalWrite(10,LOW);
+  delay(1000);
+  
+  // numero 11
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(10,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(10,LOW);  
+  delay(1000);
+  
+  // numero 12
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  delay(3000);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+  delay(1000);
+  
+  // numero 13
+  digitalWrite(13,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);  
+  delay(1000);
+  
+  // numero 14
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  delay(3000);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);  
+  delay(1000);
+  
+  // numero 15
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,HIGH);
+  delay(3000);
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);  
+  delay(1000);
+  
+  
+  
+  
+  
+}
