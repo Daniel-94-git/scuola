5c1f6b3d5d98be67ad0f19f7b9405ab43ab2812f
Daniel Salvador <daniel.salvador@rosabianca.tn.it>
2025-04-08 22:59:17 +0200

    Classe con sottoclasse java

---
+++ java/CorsoJava/src/Lab2_Inf_52/Animale.java
@@ -0,0 +1,70 @@
+package Lab2_Inf_52;
+
+// Classe genitore (superclasse)
+class Animale {
+    public String nome;
+    protected String tipo;// Accessibile a questa classe e alle sue sottoclassi
+    protected String soprannome;
+    private int eta;
+
+    public Animale(String nome, String tipo,String soprannome, int eta) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.soprannome = soprannome;
+        this.eta = eta;
+    }
+
+    public String getSoprannome() {
+        return soprannome;
+    }
+
+    public void setSoprannome(String soprannome) {
+        this.soprannome = soprannome;
+    }
+
+    public void faiVerso() {
+        System.out.println("L'animale fa un verso generico.");
+    }
+
+    // Metodo per accedere all'attributo protetto (dalla stessa classe)
+    public String getTipoAnimale() {
+        return this.tipo;
+    }
+
+    // Metodo per accedere all'attributo privato (solo da questa classe)
+    public int getEtaAnimale() {
+        return this.eta;
+    }
+
+    // Metodo per modificare l'attributo privato (solo da questa classe)
+    public void setEtaAnimale(int eta) {
+        this.eta = eta;
+    }
+}
+
+// Sottoclasse (classe figlia) che estende Animale
+class Cane extends Animale {
+    private String razza;
+
+    public Cane(String nome, String razza ,String soprannome,int eta) {
+        // Chiama il costruttore della superclasse usando super()
+        super(nome, "mammifero",soprannome,eta);
+        this.razza = razza;
+    }
+
+    @Override
+    public void faiVerso() {
+        System.out.println("Il cane abbaia: Bau!");
+        // Posso accedere all'attributo 'tipo' ereditato perché è protected
+        System.out.println("È un " + this.tipo);
+        // Non posso accedere direttamente a 'eta' perché è private nella superclasse
+        // Devo usare il metodo pubblico fornito dalla superclasse:
+        System.out.println("Ha " + getEtaAnimale() + " anni.");
+    }
+
+    public String getRazza() {
+        return this.razza;
+    }
+}
+
+
---
+++ java/CorsoJava/src/Lab2_Inf_52/Main.java
@@ -0,0 +1,20 @@
+package Lab2_Inf_52;
+
+public class Main {
+    public static void main(String[] args) {
+        Animale animaleGenerico = new Animale("Ignoto", "sconosciuto", "wow",5);
+        Cane fido = new Cane("Fido", "Labrador","cico",3);
+
+        System.out.println("--- Animale Generico ---");
+        System.out.println("Nome: " + animaleGenerico.nome);
+        System.out.println("Tipo (tramite metodo): " + animaleGenerico.getTipoAnimale());
+        // System.out.println("Tipo (accesso diretto - ERRORE): " + animaleGenerico.tipo); // Non accessibile direttamente da fuori
+        animaleGenerico.faiVerso();
+
+        System.out.println("\n--- Cane Fido ---");
+        System.out.println("Nome: " + fido.nome + fido.soprannome); // Ereditato e public
+        System.out.println("Tipo (tramite metodo ereditato): " + fido.getTipoAnimale()); // Ereditato e accessibile tramite metodo public
+        fido.faiVerso();
+        System.out.println("Razza: " + fido.getRazza());
+    }
+}
